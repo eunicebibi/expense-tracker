3b15b0483bef052172b6452161aaad338d9ea6fd
eunicebibi <agjay410@gmail.com>
2023-04-28 11:11:27 +0800

    feat: add views main and index.hbs

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port ='3000'
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 
 app.get('/', (req, res) => {
   res.send('hello world')
