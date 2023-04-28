d443a2b402d01ff0d4be76d53a9d859c54fc7dc5
eunicebibi <agjay410@gmail.com>
2023-04-28 11:31:04 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
 const port ='3000'
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+mongoose.connect(process.env.MONGODB_URI)
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 
 app.get('/', (req, res) => {
