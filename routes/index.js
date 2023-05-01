ed27b9e18bafe9f2cc8708bad53eec4c4e78f53f
eunicebibi <agjay410@gmail.com>
2023-05-01 11:34:21 +0800

    feat: add user authentication in detail filter and home routers

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const { authenticator } = require('../middleware/auth')
 const home = require('./modules/home')
-const details = require('../routes/modules/details')
+const details = require('./modules/details')
+const filter = require('./modules/filter')
 const users = require('./modules/users')  
+ 
 
-router.use('/', home)
-router.use('/details', details)
-router.use('/users', users) 
+
+router.use('/users', authenticator, users) 
+router.use('/details', authenticator, details)
+router.use('/filter', authenticator, filter)
+router.use('/', authenticator, home)
 
 
 module.exports = router
