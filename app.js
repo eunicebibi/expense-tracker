c0fc78da2062bcbb3b820b5099d45cddf45bbfc0
eunicebibi <agjay410@gmail.com>
2023-05-01 10:18:10 +0800

    feat: add mongoose.js and move code from app.js to monggose

--- app.js
+++ app.js
@@ -1,30 +1,16 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 const bodyParser = require('body-parser')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+require('./config/mongoose')
+
 const port ='3000'
 const app = express()
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.set('strictQuery', false)
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
