83128e129f184a685a8c674b5a4c88b25b55fc7e
eunicebibi <agjay410@gmail.com>
2023-05-01 11:45:33 +0800

    feat: add relations between User and Detail models

--- routes/modules/details.js
+++ routes/modules/details.js
@@ -13,6 +13,8 @@ router.get('/new', (req, res) => {
 //儲存新增
 router.post('/', (req, res) => {
   // console.log(req.body)
+  const userId = req.user._id
+  const _id = req.params.id
   const detailsData = {
     categoryId: req.body.categoryId,
     userId: req.user._id,
@@ -27,16 +29,18 @@ router.post('/', (req, res) => {
 
 //修改
 router.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Detail.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Detail.findByOne({_id, userId})
     .lean()
     .then((detail) => res.render('edit', { detail }))
     .catch(err => console.log(err))
 })
 
 router.put('/:id', (req, res) => {
-  const _id = req.params.id
   const userId = req.user._id
+  const _id = req.params.id
+  const { name, isDone } = req.body
   const detailsData = {
     name: req.body.name,
     date: req.body.date,
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,7 +4,9 @@ const Detail = require('../../models/detail')
 const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
+  const userId = req.user._id   
   Detail.find().lean()
+    .sort({ _id: 'asc' })
     .then(details => {
       //transform date format to YYYY/MM/DD
       return details.map((detail) => {
