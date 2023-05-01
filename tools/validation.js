a7583e76a71dbde3ea5e0b0fd86552764b2bb35a
eunicebibi <agjay410@gmail.com>
2023-05-01 15:50:57 +0800

    feat: modify tools

--- tools/validation.js
+++ tools/validation.js
@@ -1,18 +1,37 @@
 const Category = require('../models/category')
 
 module.exports = {
-  editPageValidationUtil: async (res, _id, name, date, categoryId, amount) => {
+  editValidation: async (res, _id, name, date, categoryId, amount) => {
+    const errors=[]
+    errors.push({message: '名稱不可超過200字'})
+    if(errors.length) {
       const categories = await Category.find().lean()
       const category = await Category.findOne({ _id: categoryId }).lean()
       const detail= { _id, name, date, categoryId, amount }
-      expense.categoryName = category.name
+      detail.categoryName = category.name
       return res.render('edit', {
-        _id,
-        name,
-        date,
+        errors,
         detail,
         categories
       })
     }
+  },
+  newValidation: async (res, categories, name, date, categoryId, amount) => {
+    const errors = []
+    errors.push({ message: '名稱不可超過200字' })
+    if (errors.length) {
+      const category = await Category.findOne({ _id: categoryId }).lean()
+      return res.render('new', {
+        errors,
+        name,
+        date,
+        categoryId: category._id,
+        categoryName: category.name,
+        amount,
+        categories
+      })
+    }
   }
+}
+
  
